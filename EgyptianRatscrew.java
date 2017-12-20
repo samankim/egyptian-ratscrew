da8ee7432a1ef507ca45247bba672f8372fb4fcc
samankim <33303898+samankim@users.noreply.github.com>
2017-12-19 21:38:36 -0500

    Update EgyptianRatscrew.java

--- EgyptianRatscrew.java
+++ EgyptianRatscrew.java
@@ -1,9 +1,6 @@
 /*
  * File: EgyptianRatscrew.java
  * ---------------------
- * This class is a blank one that you can change at will. Remember, if you change
- * the class name, you'll need to change the filename so that it matches.
- * Then you can extend GraphicsProgram, ConsoleProgram, or DialogProgram as you like.
  */
 
 import acm.graphics.*;
